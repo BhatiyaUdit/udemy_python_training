99411694371eee269cafb7fd9d5c5715296313b2
Udit Bhatiya <bhatiyaUdit@johnDeere.com>
2021-11-14 18:45:58 +0530

    Added HTML File

---
+++ New_Section32_APP9_DjangoBlogTranslatorApp/277_Create_model.py
@@ -0,0 +1,13 @@
+"""
+
+Create a class that will inherit from models.Model
+Add fields and related properties
+
+refer to models.py for all details
+
+Commands that need to be executed
+>> python manage.py makemigrations  -> will generate SQLS
+>> python manage.py migrate
+
+
+"""
--- New_Section32_APP9_DjangoBlogTranslatorApp/blog/models.py
+++ New_Section32_APP9_DjangoBlogTranslatorApp/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, 'DRAFT'), (1, "PUBLISH"))
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+
