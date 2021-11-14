afe097d1894004338049952c521dcab663c4b57b
Udit Bhatiya <bhatiyaUdit@johnDeere.com>
2021-11-14 19:00:16 +0530

    Working version

--- New_Section32_APP9_DjangoBlogTranslatorApp/blog/urls.py
+++ New_Section32_APP9_DjangoBlogTranslatorApp/blog/urls.py
@@ -1,6 +1,6 @@
-import views
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('<slug>:<slug>', views.BlogView.as_view(), name='blog_pattern')
+    path('<slug:slug>', views.BlogView.as_view(), name='blog_pattern')
 ]
