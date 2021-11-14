0058ffed4a71576a36162028609ab9ba717db353
Udit Bhatiya <bhatiyaUdit@johnDeere.com>
2021-11-14 18:51:11 +0530

    Added A View file connecting model and template

--- New_Section32_APP9_DjangoBlogTranslatorApp/blog/views.py
+++ New_Section32_APP9_DjangoBlogTranslatorApp/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Posts
+from django.views.generic import DetailView
+
 
 # Create your views here.
+
+# # lecture 280 Adding views
+
+class BlogView(DetailView):
+    model = Posts
+    template_name = "blog.html"
