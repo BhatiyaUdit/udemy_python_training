077e6348eabdc2aa6abd38fdc33a1fb03423cff3
Udit Bhatiya <bhatiyaUdit@johnDeere.com>
2021-11-17 16:50:16 +0530

    Home page added

--- New_Section32_APP9_DjangoBlogTranslatorApp/blog/urls.py
+++ New_Section32_APP9_DjangoBlogTranslatorApp/blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 print("Inside blog >> urls.py ")
 
 urlpatterns = [
-    path('<slug:slug>', views.BlogView.as_view(), name='blog_pattern')
+    path('<slug:slug>', views.BlogView.as_view(), name='blog_pattern'),
+    path("", views.HomeView.as_view(), name="home_view_pattern")
 ]
--- New_Section32_APP9_DjangoBlogTranslatorApp/blog/views.py
+++ New_Section32_APP9_DjangoBlogTranslatorApp/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Posts
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 
 print("Inside blog >> views.py ")
 
@@ -13,3 +13,7 @@ class BlogView(DetailView):
     print("inside Blog View Class")
     model = Posts
     template_name = "blog.html"
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
